a701ef90670b6cfe092a23ff9e95d6bc970fbd6c
Meitar Keshet <meitarkeshet@gmail.com>
2021-05-12 18:32:08 +0200

    fixed run map once on load

--- js/mapall.js
+++ js/mapall.js
@@ -26,103 +26,104 @@ var layerGroup = L.layerGroup().addTo(mymap);
 $(function() {
     var map_reload = function() {
         alert('yo');
-    };
-    $("#button").click(your_code);
-    your_code();
-});
-
-$(document).ready(function() {
-    $grid.on('arrangeComplete', // 
-        function(event, filteredItems) {
-            console.log('Isotope arrange completed on ' +
-                filteredItems.length + ' items');
-
-            // empty string to push to
-            var string_combined_arr = []
-                // for every picture (both visible and invisible?)
-            $(".building_square").each(function(index) {
-                //console.log(index + ": " + $(this).text().split('\n')[1]); // use to get specific line form text 
-                var obj = new Object();
-                obj.type = "Feature";
-                // dict prop.
-                var properties = {};
-                let id = $(this).attr('id');
-                //alert(id);
-                // address?
-                properties.name = "id"; // $(this).attr('id')
-                if ($(this).css('display') == 'none') { // if the image is not currently showing (was filterd by user, don't )
-                    properties.show_on_map = false;
-                } else {
-                    properties.show_on_map = true;
-                };
-
-
-                obj.properties = properties;
-                // add popupContent?
-                // dict geo.
-                var geometry = {};
-                geometry.type = "Point";
-                let lon = parseFloat($(this).text().split('\n')[2].trim()); // grab the long column (as string) and trim the excess spaces and parse to float
-                let lat = parseFloat($(this).text().split('\n')[3].trim());
-                let cord = [lon, lat]
-                geometry.coordinates = cord; // format [-104.98404, 39.74621]
-                obj.geometry = geometry;
-                // push to combined string
-                string_combined_arr.push(obj);
-                //convert object to json string
-                var tmp_string = JSON.stringify(obj);
-                //console.log(JSON.parse(tmp_string)); // this is your requirement.
-            });
-            var str_open = '{ "type": "FeatureCollection",' +
-                '"features": '; // notice no multiline option in JS
-            var str_close = '}'
-            var string_combined_str = JSON.stringify(string_combined_arr); // turn from array to JSON formatted string
-            var together = str_open + string_combined_str + str_close;
-            var json_pins = JSON.parse(together);
-
-            mymap.invalidateSize() // fix gray areas issue after initializing with height 0
-
-
-            // to download and see the json created
-            var a = document.createElement("a")
-            a.href = `data:application/json;charset=utf-8, ${together}`
-            a.download = "some.json"
-            document.body.appendChild(a)
-            a.click()
-            document.body.removeChild(a)
-
-
-            //alert(locations_layer);
-            //$('#mapid').remove();
-
-
-            if (layerGroup !== undefined) {
-                // remove all the markers in one go
-                layerGroup.clearLayers();
-                mymap.removeLayer(layerGroup);
-                // alert("trying to clean");
+        // empty string to push to
+        var string_combined_arr = []
+            // for every picture (both visible and invisible?)
+        $(".building_square").each(function(index) {
+            //console.log(index + ": " + $(this).text().split('\n')[1]); // use to get specific line form text 
+            var obj = new Object();
+            obj.type = "Feature";
+            // dict prop.
+            var properties = {};
+            let id = $(this).attr('id');
+            //alert(id);
+            // address?
+            properties.name = "id"; // $(this).attr('id')
+            if ($(this).css('display') == 'none') { // if the image is not currently showing (was filterd by user, don't )
+                properties.show_on_map = false;
+            } else {
+                properties.show_on_map = true;
             };
 
 
-            locations_layer = new L.geoJSON(json_pins, {
-                filter: function(feature, layer) {
-                    return feature.properties.show_on_map;
-                },
-                pointToLayer: function(feature, latlng) {
-                    return new L.CircleMarker(latlng, {
-                        radius: 10,
-                        fillOpacity: 0.85
-                    });
-                }
-            });
-            // Adding current locations to the layer group
-            locations_layer.addTo(layerGroup);
+            obj.properties = properties;
+            // add popupContent?
+            // dict geo.
+            var geometry = {};
+            geometry.type = "Point";
+            let lon = parseFloat($(this).text().split('\n')[2].trim()); // grab the long column (as string) and trim the excess spaces and parse to float
+            let lat = parseFloat($(this).text().split('\n')[3].trim());
+            let cord = [lon, lat]
+            geometry.coordinates = cord; // format [-104.98404, 39.74621]
+            obj.geometry = geometry;
+            // push to combined string
+            string_combined_arr.push(obj);
+            //convert object to json string
+            var tmp_string = JSON.stringify(obj);
+            //console.log(JSON.parse(tmp_string)); // this is your requirement.
+        });
+        var str_open = '{ "type": "FeatureCollection",' +
+            '"features": '; // notice no multiline option in JS
+        var str_close = '}'
+        var string_combined_str = JSON.stringify(string_combined_arr); // turn from array to JSON formatted string
+        var together = str_open + string_combined_str + str_close;
+        var json_pins = JSON.parse(together);
 
-            // Adding layer group to map
-            layerGroup.addTo(mymap);
-        }
-    ); // close isotope arrange
+        mymap.invalidateSize() // fix gray areas issue after initializing with height 0
+
+
+        // to download and see the json created
+        var a = document.createElement("a")
+        a.href = `data:application/json;charset=utf-8, ${together}`
+        a.download = "some.json"
+        document.body.appendChild(a)
+        a.click()
+        document.body.removeChild(a)
+
+
+        //alert(locations_layer);
+        //$('#mapid').remove();
+
+
+        if (layerGroup !== undefined) {
+            // remove all the markers in one go
+            layerGroup.clearLayers();
+            mymap.removeLayer(layerGroup);
+            // alert("trying to clean");
+        };
+
+
+        locations_layer = new L.geoJSON(json_pins, {
+            filter: function(feature, layer) {
+                return feature.properties.show_on_map;
+            },
+            pointToLayer: function(feature, latlng) {
+                return new L.CircleMarker(latlng, {
+                    radius: 10,
+                    fillOpacity: 0.85
+                });
+            }
+        });
+        // Adding current locations to the layer group
+        locations_layer.addTo(layerGroup);
+
+        // Adding layer group to map
+        layerGroup.addTo(mymap);
+    };
+    map_reload(); // run one on doc start;
+    $(document).ready(function() { // run on every change in grid;
+        $grid.on('arrangeComplete', // 
+            function(event, filteredItems) {
+                console.log('Isotope arrange completed on ' +
+                    filteredItems.length + ' items');
+                map_reload();
+            });
+    });
 });
+
+
+
+
 
 /*
 geojsonLayer = L.geoJson(json_pins, {
